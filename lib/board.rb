36da66dfd9b79cc59c5c78074fc4a5a761494ed8
Liam Cusack <LCusack8108@gmail.com>
2020-10-14 20:28:14 -0600

    Add valid_placement method

--- lib/board.rb
+++ lib/board.rb
@@ -2,14 +2,33 @@ require 'pry'
 
 class Board
   attr_reader :cells
+
    def initialize(cells = {})
      @cells = cells
    end
+
    def cells
-     cell_names = ["A1", "A2", " A3", "A4", "B1", "B2","B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+     cell_names = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
      cell_names.map do |name|
        @cells["#{name}"] = Cell.new(name)
      end
      @cells
+   end
+
+   def valid_coordinate?(cell)
+    self.cells.key?(cell)
+   end
+
+   def valid_placement?(ship, coordinates = [])
+     ship.length == coordinates.count
+     ord_values = []
+     coordinates.map do |coordinate|
+     ord_values << coordinate.split
+     ord_values.ord
+   end
+   if ord_values.max - ord_values.min < ship.length && ship.length == coordinates.count
+   else
+     false
+   end
    end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -8,16 +8,30 @@ require 'pry'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
-
     assert_instance_of Board, @board
   end
 
   def test_it_has_cells
   assert_instance_of Hash, @board.cells
   assert_equal 16, @board.cells.count
-  # check taht keys point to obejcts
+  end
+
+  def test_coordinates_valid
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal true, @board.valid_coordinate?("D4")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("A22")
+  end
+
+  def test_ship_array
+    # binding.pry
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 end
