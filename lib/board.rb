4176795fb6c8d88896719bd1ef6226c31415fb79
awosborn <alanturing@example.coclear>
2020-10-19 19:20:51 -0600

    Add valid placement module

---
+++ lib/board.rb
@@ -0,0 +1,63 @@
+require 'pry'
+require './lib/cell'
+require './lib/ship'
+require './lib/valid_placement'
+
+class Board
+  attr_reader :cells, :row, :columm, :status
+  include ValidPlacement
+
+  def initialize(board_size = 4)
+    @board_size = board_size
+    @row = []
+    @column = []
+    @cells = {}
+    populate_board
+  end
+
+  def populate_board
+    ('A'..'Z').each do |letter|
+      break if letter.ord > (@board_size + 64)
+      @row << letter
+    end
+    (1..(@board_size)).each do |number|
+      @column << number
+    end
+    @column.each do |number|
+      @row.each do |letter|
+        @cells["#{letter}#{number}"] = Cell.new("#{letter}#{number}")
+      end
+    end
+  end
+
+  def valid_coordinate?(cell)
+    self.cells.key?(cell)
+  end
+
+  def valid_placement?(ship, coordinates = [])
+    return false if lengths_different(ship, coordinates)
+    return false if coordinates_have_ship(coordinates)
+    return false if coordinates_not_consecutive(ship, coordinates)
+    return false if no_common_row_or_column(coordinates)
+    return true
+  end
+
+  def place(ship,coordinate)
+    if coordinate.all? do |coordinate|
+       @cells[coordinate].empty?
+     end
+   end
+   coordinate.each do |coordinate|
+     @cells[coordinate].place_ship(ship)
+   end
+ end
+
+ def render(option = nil)
+  #USE CONCAT TO MAKE IT DYNAMIC (BASED ON BOARD SIZE)
+  puts " 1 2 3 4 \n" +
+    "A #{@cells["A1"].render(option)} #{@cells["A2"].render(option)} #{@cells["A3"].render(option)} #{@cells["A4"].render(option)}\n" +
+    "B #{@cells["B1"].render(option)} #{@cells["B2"].render(option)} #{@cells["B3"].render(option)} #{@cells["B4"].render(option)}\n" +
+    "C #{@cells["C1"].render(option)} #{@cells["C2"].render(option)} #{@cells["C3"].render(option)} #{@cells["C4"].render(option)}\n" +
+    "D #{@cells["D1"].render(option)} #{@cells["D2"].render(option)} #{@cells["D3"].render(option)} #{@cells["D4"].render(option)}\n"
+  end
+end
--- lib/valid_placement.rb
+++ lib/valid_placement.rb
@@ -1,71 +1,49 @@
-# class Valid_placement
-#
-#
-# def initialize(ship, coordinates)
-#   @ship = ship
-#   @coordinates = []
-# end
-#
-# def same_length_as_ship(ship, coordinates)
-#   ship.count == coordinates.count
-# end
-#
-# def coordinates_consecutive(coordinates)
-#   #either numbers or letters are the same then check the other
-#   letters = coordinates.map do |coordinate|
-#     coordinate.ord
-#   end
-#
-#   numbers = coordinates.map do |coordinate|
-#     coordinate.slice(1).to_i
-#   end
-#
-#   if numbers.uniq.length == 1
-#     letters.each_cons(2).all? do |letter|
-#       p letter
-#     end
-#   elsif letters.uniq.length == 1
-#     numbers.each_cons(2).all? do |number|
-#       p number
-#     end
-#   else return false
-#   end
-# end
-#
-#
-# # CHANGE LETTERS TO ASCII
-# letters = coordinates.map do |coordinate|
-#   coordinate.ord
-# end
-# # CHANGE NUMBERS TO ascii
-# numbers = coordinates.map do |coordinate|
-#   coordinate.slice(1).to_i
-# end
-# # CHANGE LETTER+NUMBER TO ASCII
-# ascii = coordinates.map do |coordinate|
-#   coordinate.bytes.inject(:+)
-# end
-# # CHECK IF HAS SHIP
-# return false if coordinates.any? do |coordinate|
-#   @cells[coordinate].ship
-# end
-#
-# # CHECK IF RIGHT NUMBER OF SPACES
-# return false if ship.length != coordinates.length
-# # CHECK EITHER NUMS OR LETTERS ARE SAME
-# return false if numbers.uniq.length !=1 && letters.uniq.length !=1
-# # CHECK IF LETTERS SAME and NUMS CONS
-# return false if (letters.uniq.length == 1) &&
-# !(numbers.each_cons(2).all? do |number|
-#   p number
-# end)
-# # CHECK IF NUMS SAME AND LETTERS CONS
-# return false if (numbers.uniq.length == 1) &&
-# !(letters.each_cons(2).all? do |letter|
-#   p letter
-# end)
-# # CHECK IF CONSECUTIVE, BUT NOT DIAGONAL
-# return false if (ascii.last - ascii.first) != ship.length - 1 && (ascii.max - ascii.min) != ship.length - 1
-# return true
-#
-# end
+module ValidPlacement
+
+  def lengths_different(ship, coordinates)
+    return ship.length != coordinates.length
+  end
+
+  def coordinates_have_ship(coordinates)
+    coordinates.any? do |coordinate|
+      @cells[coordinate].ship
+    end
+  end
+
+  def no_common_row_or_column(coordinates)
+    letters = coordinates.map do |coordinate|
+      coordinate.ord
+    end
+    numbers = coordinates.map do |coordinate|
+      coordinate.slice(1).to_i
+    end
+    letters.uniq.length != 1 && numbers.uniq.length != 1
+  end
+
+  def coordinates_not_consecutive(ship, coordinates)
+    #separate letters
+    letters = coordinates.map do |coordinate|
+      coordinate.ord
+    end
+    #separate numbers
+    numbers = coordinates.map do |coordinate|
+      coordinate.slice(1).to_i
+    end
+    # get ascii
+    ascii = coordinates.map do |coordinate|
+      coordinate.bytes.inject(:+)
+    end
+    # return false if numbers same and letters cons
+    return false if (letters.uniq.length == 1) &&
+      numbers.each_cons(2).all? do |num1, num2|
+        num1 == num2 - 1
+      end
+      # return false if letters same and nums cons
+    return false if (numbers.uniq.length == 1) &&
+      letters.each_cons(2).all? do |let1, let2|
+        let1 == let2-1
+      end
+    return false if (ascii.last - ascii.first) == ship.length - 1 && (ascii.max - ascii.min) == ship.length - 1
+    return true
+  end
+end
