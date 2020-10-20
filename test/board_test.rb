9d4cc4b7c85354aaa0c1fbc65df17e631b4bdeae
Adam Etzion <adametzion90@gmail.com>
2020-10-20 14:55:48 -0600

    Add Board test fix AND remove puts from board render method

--- test/board_test.rb
+++ test/board_test.rb
@@ -31,7 +31,7 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A22")
   end
 
-  def test_ship_array
+  def test_ship_lenggth_matches_coordinates_length
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
@@ -43,51 +43,48 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
   end
 
-  def test_coordinates_diagonal
+  def test_coordinates_not_diagonal
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "C2"])
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A4", "C2"])
     assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_coordinates_valid
 
     assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
-
     assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
-  def test_edge_case
-
+  def test_edge_case_valid_placement
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "C2"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A4", "C2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "C1"])
   end
 
-  def test_it_can_place
-  @board.place(@cruiser, ["A1", "A2", "A3"])
-  assert_equal @board.cells["A1"], @board.cells["A1"]
-  assert_equal @board.cells["A2"], @board.cells["A2"]
-  assert_equal @board.cells["A3"], @board.cells["A3"]
-  end
+  def test_it_can_place_ship
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"] 
+    cell_2 = @board.cells["A2"] 
+    cell_3 = @board.cells["A3"] 
 
-  def test_it_has_the_same_ship
-  @board.place(@cruiser, ["A1", "A2", "A3"])
-
-  assert_equal true, @board.cells["A3"].ship == @board.cells["A2"].ship
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert_equal @cruiser, cell_3.ship
+    assert_equal cell_2.ship, cell_3.ship
   end
 
   def test_it_already_has_a_ship
-  @board.place(@cruiser, ["A1", "A2", "A3"])
-
-
-  assert_equal false, @board.valid_placement?(@submarine, ["A1","B1"])
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    
+    assert_equal false, @board.valid_placement?(@submarine, ["A1","B1"])
   end
 
   def test_it_can_render_the_board
     @board.place(@cruiser, ["A1", "A2", "A3"])
-    expected =  "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
-    expected2 = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
 
+    expected = " 1 2 3 4  \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
+    expected2 = " 1 2 3 4  \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
+    binding.pry
     assert_equal expected, @board.render()
     assert_equal expected2, @board.render(true)
   end
