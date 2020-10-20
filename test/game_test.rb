9f172ec7ee769dde28d288e9be2af57d5367f255
awosborn <alanturing@example.coclear>
2020-10-20 15:33:25 -0600

    Add format changes to GameTest

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,13 +6,11 @@ require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/game'
-require './lib/valid_placement'
-require './lib/messages'
-require 'pry'
+
+
 
 class GameTest < Minitest::Test
-  # include ValidPlacement
-  # include Messages
+
   def setup
     @game = Game.new
 
@@ -22,19 +20,12 @@ class GameTest < Minitest::Test
 
     assert_instance_of Game, @game
   end
-
-
+x
   def test_victory
     @game.victory
     expected = "You weren't worthy... Come back if you dare!"
 
     assert_equal expected, @game.victory
-  end
-
-  def test_it_can_setup_pc_board
-      @game.setup_pc_board
-
-      assert_equal true, @pc.board.valid_placement?(@pc.submarine, pc_coord_2)
   end
 
 end
