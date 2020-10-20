2548f09e5523acd81667dc88f70c81f4d199ce44
awosborn <alanturing@example.coclear>
2020-10-20 17:05:58 -0600

    Move pc board setup to pc class

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
 
-class Test < Minitest::Test
+class ShipTest < Minitest::Test
   def setup
     @cruiser = Ship.new("Cruiser", 3)
   end
